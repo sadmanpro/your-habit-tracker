96e550d0c38ef921f920b6147baa57de1d55c2f5
SADMAN PRODHAN <sadmanpro34@gmail.com>
2026-01-28 04:10:33 +0000

    remove hover zoom

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-md transition-all duration-300 hover:shadow-xl hover:-translate-y-1 hover:scale-[1.01]",
+      "rounded-lg border bg-card text-card-foreground shadow-md transition-all duration-300",
       className
     )}
     {...props}
